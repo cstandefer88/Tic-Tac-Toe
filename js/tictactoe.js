5b5c125c3fe4e294267ea6a1be8d5b5dafb354e3
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-04-01 12:32:04 -0500

    2nd attempt at initial JS psuedocode

---
+++ js/tictactoe.js
@@ -0,0 +1,15 @@
+// Click button and display sarcastic prompt
+
+// Computer's Random Math Move Retrieval System
+
+// Game's rule setup
+
+// Player Move with 5 second timer.
+
+// Computer Move
+
+// Loop between player and computer until one wins
+
+// Decide winner
+
+// Play to Five
